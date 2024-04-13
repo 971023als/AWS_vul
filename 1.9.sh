3ea9c126ed240b694f1ffa5213c06b57f0a8ff92
971023als <971023als@naver.com>
2024-04-13 17:18:31 +0900

    1.10

--- 1.9.sh
+++ 1.9.sh
@@ -1,46 +1,31 @@
 #!/bin/bash
 
-# 변수 설정
-분류="파일 및 디렉터리 관리"
-코드="U-09"
-위험도="상"
-진단_항목="/etc/hosts 파일 소유자 및 권한 설정"
-대응방안="/etc/hosts 파일의 소유자가 root이고, 권한이 600 이하인 경우"
-hosts_file='/etc/hosts'
+# 변수 초기화
+분류="계정 관리"
+코드="1.9"
+위험도="중요도 중"
+진단_항목="MFA (Multi-Factor Authentication) 설정"
+대응방안="AWS Multi-Factor Authentication(MFA)은 사용자 이름과 암호 외에 보안을 한층 더 강화할 수 있는 방법으로, MFA를 활성화하면 사용자가 AWS 웹 사이트에 로그인할 때 사용자 이름과 암호뿐만 아니라 AWS MFA 디바이스의 인증 응답을 입력하라는 메시지가 표시됩니다. 이러한 다중 요소를 통해 AWS 계정 설정 및 리소스에 대한 보안을 높일 수 있습니다."
+설정방법="MFA 인증 설정 및 확인: 1) IAM 메인 → 우측상단 계정 → 내 보안 자격 증명 → 멀티 팩터 인증 → MFA 활성화, 2) MFA 디바이스 관리 → 가상 MFA 디바이스 선택 → 계속, 3) Google OTP 어플 설치 → ‘+’ 버튼 → 바코드 스캔 → 나타난 QR코드를 어플에서 스캔, 4) 스캔 후 나타난 숫자 MFA 코드 1 입력 → 재 생성된 숫자 MFA 코드 2 입력, 5) 2개의 연속된 MFA 코드 입력, 6) MFA 설정 완료, 7) 로그인 시 비밀번호 입력, 8) Google OTP 번호 입력 후 로그인 시도, 9) 로그인 확인"
 현황=()
 진단_결과=""
 
-# /etc/hosts 파일 존재 여부 확인
-if [ -e "$hosts_file" ]; then
-    # 파일 권한 및 소유자 확인
-    mode=$(stat -c "%a" "$hosts_file")
-    owner_uid=$(stat -c "%u" "$hosts_file")
-
-    # 소유자가 root이고 권한이 600 이하인지 확인
-    if [ "$owner_uid" -eq 0 ]; then
-        if [ "$mode" -le 600 ]; then
-            진단_결과="양호"
-            현황+=("/etc/hosts 파일의 소유자가 root이고, 권한이 $mode입니다.")
-        else
-            진단_결과="취약"
-            현황+=("/etc/hosts 파일의 권한이 $mode로 설정되어 있어 취약합니다.")
-        fi
-    else
-        진단_결과="취약"
-        현황+=("/etc/hosts 파일의 소유자가 root가 아닙니다.")
-    fi
+# MFA 상태 확인
+mfa_status=$(aws iam list-mfa-devices --user-name <IAM_USER_NAME> --query 'MFADevices' --output json)
+if [ -z "$mfa_status" ]; then
+    echo "No MFA devices configured for the user."
+    진단_결과="취약"
 else
-    진단_결과="N/A"
-    현황+=("/etc/hosts 파일이 없습니다.")
+    echo "MFA is enabled for the user."
+    진단_결과="양호"
 fi
 
 # 결과 출력
 echo "분류: $분류"
 echo "코드: $코드"
 echo "위험도: $위험도"
-echo "진단 항목: $진단_항목"
+echo "진단_항목: $진단_항목"
 echo "대응방안: $대응방안"
-echo "진단 결과: $진단_결과"
-for item in "${현황[@]}"; do
-    echo "현황: $item"
-done
+echo "설정방법: $설정방법"
+echo "현황: ${현황[@]}"
+echo "진단_결과: $진단_결과"
